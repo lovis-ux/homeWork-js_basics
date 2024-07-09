af7971e989a7acf6d14850ad96f1c919719f311d
lovis <vlasovva444@gmail.com>
2024-07-09 13:23:46 +0300

    ДЗ оновы

---
+++ app.js
@@ -0,0 +1,10 @@
+let adressLat;
+let adressLong;
+let positionLat;
+let positionLong;
+
+let template1 = adressLat * positionLong / 2;
+let template2 = positionLat * adressLong / 2;
+
+
+// не понятно что надо выводить и куда, формула должна быть правильная
